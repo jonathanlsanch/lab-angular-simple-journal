e159de9fcf135fd409e594a86417a7943053695a
Jonathan Sanchez <jonathansanchez@Jonathans-MacBook-Pro.local>
2018-03-09 10:25:10 -0500

    Finish iteration 1 and 2

--- starter-code/client/src/app/app.module.ts
+++ starter-code/client/src/app/app.module.ts
@@ -2,19 +2,30 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { JournalService } from './journal.service';
+
+// Routes
+import { Routes } from '@angular/router';
+import { routes } from './app.routing';
+
+import { RouterModule } from "@angular/router";
+
 
 import { AppComponent } from './app.component';
+import { EntryListComponent } from './entry-list/entry-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EntryListComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    RouterModule.forRoot(routes)
   ],
-  providers: [],
+  providers: [JournalService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
